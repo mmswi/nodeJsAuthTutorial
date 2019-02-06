b0d5538eed80ca9415dd117016f67c6f20481b7b
Mihai Marinescu <mihai.marinescu@vivre.eu>
2019-02-06 08:25:33 +0200

    Added pug and express

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard");
+});
+
+app.listen(3000);
+
+app.on("listening", () => {
+    console.log("server listening on port 3000")
+})
